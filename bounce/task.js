e8989e3446026860647959fb2a60a4f7e8624d63
waleedzidan2020 <83763887+waleedzidan2020@users.noreply.github.com>
2022-05-20 20:16:30 +0200

    Add files via upload

---
+++ bounce/task.js
@@ -0,0 +1,21 @@
+$("#card1").mouseover(function(){
+    $("#card1").css("opacity","1")
+})
+
+$("#card1").mouseout(function(){
+    $("#card1").css("opacity","0.4")
+})
+$("#card2").mouseover(function(){
+    $("#card2").css("opacity","1")
+})
+
+$("#card2").mouseout(function(){
+    $("#card2").css("opacity","0.4")
+})
+$("#card3").mouseover(function(){
+    $("#card3").css("opacity","1")
+})
+
+$("#card3").mouseout(function(){
+    $("#card3").css("opacity","0.4")
+})
